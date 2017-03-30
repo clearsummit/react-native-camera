976c579ca601b5a46a386cc76a1b1a77969146e8
JunoCS <juno@clearsumm.it>
2017-03-30 12:13:21 +0700

    Implementation in iOS: - add support for `preview` to match viewport cropping - create a thumbnail on video and send it to app

--- ios/RCTCameraManager.h
+++ ios/RCTCameraManager.h
@@ -4,56 +4,57 @@
 @class RCTCamera;
 
 typedef NS_ENUM(NSInteger, RCTCameraAspect) {
-  RCTCameraAspectFill = 0,
-  RCTCameraAspectFit = 1,
-  RCTCameraAspectStretch = 2
+    RCTCameraAspectFill = 0,
+    RCTCameraAspectFit = 1,
+    RCTCameraAspectStretch = 2
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraCaptureSessionPreset) {
-  RCTCameraCaptureSessionPresetLow = 0,
-  RCTCameraCaptureSessionPresetMedium = 1,
-  RCTCameraCaptureSessionPresetHigh = 2,
-  RCTCameraCaptureSessionPresetPhoto = 3,
-  RCTCameraCaptureSessionPreset480p = 4,
-  RCTCameraCaptureSessionPreset720p = 5,
-  RCTCameraCaptureSessionPreset1080p = 6
+    RCTCameraCaptureSessionPresetLow = 0,
+    RCTCameraCaptureSessionPresetMedium = 1,
+    RCTCameraCaptureSessionPresetHigh = 2,
+    RCTCameraCaptureSessionPresetPhoto = 3,
+    RCTCameraCaptureSessionPreset480p = 4,
+    RCTCameraCaptureSessionPreset720p = 5,
+    RCTCameraCaptureSessionPreset1080p = 6,
+    RCTCameraCaptureSessionPresetPreview = 7
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraCaptureMode) {
-  RCTCameraCaptureModeStill = 0,
-  RCTCameraCaptureModeVideo = 1
+    RCTCameraCaptureModeStill = 0,
+    RCTCameraCaptureModeVideo = 1
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraCaptureTarget) {
-  RCTCameraCaptureTargetMemory = 0,
-  RCTCameraCaptureTargetDisk = 1,
-  RCTCameraCaptureTargetTemp = 2,
-  RCTCameraCaptureTargetCameraRoll = 3
+    RCTCameraCaptureTargetMemory = 0,
+    RCTCameraCaptureTargetDisk = 1,
+    RCTCameraCaptureTargetTemp = 2,
+    RCTCameraCaptureTargetCameraRoll = 3
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraOrientation) {
-  RCTCameraOrientationAuto = 0,
-  RCTCameraOrientationLandscapeLeft = AVCaptureVideoOrientationLandscapeLeft,
-  RCTCameraOrientationLandscapeRight = AVCaptureVideoOrientationLandscapeRight,
-  RCTCameraOrientationPortrait = AVCaptureVideoOrientationPortrait,
-  RCTCameraOrientationPortraitUpsideDown = AVCaptureVideoOrientationPortraitUpsideDown
+    RCTCameraOrientationAuto = 0,
+    RCTCameraOrientationLandscapeLeft = AVCaptureVideoOrientationLandscapeLeft,
+    RCTCameraOrientationLandscapeRight = AVCaptureVideoOrientationLandscapeRight,
+    RCTCameraOrientationPortrait = AVCaptureVideoOrientationPortrait,
+    RCTCameraOrientationPortraitUpsideDown = AVCaptureVideoOrientationPortraitUpsideDown
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraType) {
-  RCTCameraTypeFront = AVCaptureDevicePositionFront,
-  RCTCameraTypeBack = AVCaptureDevicePositionBack
+    RCTCameraTypeFront = AVCaptureDevicePositionFront,
+    RCTCameraTypeBack = AVCaptureDevicePositionBack
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraFlashMode) {
-  RCTCameraFlashModeOff = AVCaptureFlashModeOff,
-  RCTCameraFlashModeOn = AVCaptureFlashModeOn,
-  RCTCameraFlashModeAuto = AVCaptureFlashModeAuto
+    RCTCameraFlashModeOff = AVCaptureFlashModeOff,
+    RCTCameraFlashModeOn = AVCaptureFlashModeOn,
+    RCTCameraFlashModeAuto = AVCaptureFlashModeAuto
 };
 
 typedef NS_ENUM(NSInteger, RCTCameraTorchMode) {
-  RCTCameraTorchModeOff = AVCaptureTorchModeOff,
-  RCTCameraTorchModeOn = AVCaptureTorchModeOn,
-  RCTCameraTorchModeAuto = AVCaptureTorchModeAuto
+    RCTCameraTorchModeOff = AVCaptureTorchModeOff,
+    RCTCameraTorchModeOn = AVCaptureTorchModeOn,
+    RCTCameraTorchModeAuto = AVCaptureTorchModeAuto
 };
 
 @interface RCTCameraManager : RCTViewManager<AVCaptureMetadataOutputObjectsDelegate, AVCaptureFileOutputRecordingDelegate>
@@ -75,6 +76,7 @@ typedef NS_ENUM(NSInteger, RCTCameraTorchMode) {
 @property (nonatomic, strong) RCTPromiseResolveBlock videoResolve;
 @property (nonatomic, strong) RCTPromiseRejectBlock videoReject;
 @property (nonatomic, strong) RCTCamera *camera;
+@property (nonatomic, assign) BOOL cropToViewport;
 
 
 - (void)changeOrientation:(NSInteger)orientation;
